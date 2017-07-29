bd2c0f0188e0bf71778b7b9ceff92cea89a778de
Chris Milliano <MillianoC@gmail.com>
2017-07-29 10:12:30 -0500

    FEAT: Add a dialog module component, note functional, but not rendering at this time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { DialogService } from "./services/dialog.service";
+import { DialogModule } from './components/dialog/dialog.module';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DialogModule
   ],
   providers: [
     DialogService
