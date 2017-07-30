901ab2f41eac523c057692ec3004209f3c02551e
Chris Milliano <MillianoC@gmail.com>
2017-07-30 08:41:12 -0500

    FIX: Dialog selector changed to dialog-box since dialog is a valid tag in chrome

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'dialog',
+  selector: 'dialog-box',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
